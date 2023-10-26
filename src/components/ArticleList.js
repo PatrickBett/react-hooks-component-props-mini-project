f0772b6dc23c8db8b1180ed20f9b92433d2c1999
PatrickBett <patrickbett018@gmail.com>
2023-10-26 08:20:27 +0300

    first commit

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,9 +2,7 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({posts}) {
-    console.log(posts);
     const articles = posts.map((article) => {
-        //destructuring each article
         const {date: date, title: title, preview: preview, id: id} = article;
         return <Article key= {id} title = {title} date = {date} preview = {preview} />
     })
